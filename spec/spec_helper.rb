9246f80a829236fab44af53aa58ccf0c03a03244
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2018-12-24 13:47:32 +0100

    Do not run simplecov on CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/generated/'
+if ENV.fetch('CI', false) == false
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_filter '/generated/'
+  end
 end
 require 'bundler/setup'
 require 'rspec'
